15dda90ef044b1936d8f6cf2c544d6791efbb0b4
umair ansari <umairnsr87@gmail.com>
2020-01-21 00:16:29 +0530

    implemented signup activity

--- eprofile_app/views.py
+++ eprofile_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html',{})
+
+
+def signup(request):
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=UserCreationForm()
+
+    return render(request,'registration.html',{'form':form})
