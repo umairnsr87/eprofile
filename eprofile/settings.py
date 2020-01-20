ec4e95b31ff8658c0534812650c4bfbabcb84921
umair ansari <umairnsr87@gmail.com>
2020-01-20 23:09:06 +0530

    Updated version of the code

--- eprofile/settings.py
+++ eprofile/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'eprofile.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eprofile',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
