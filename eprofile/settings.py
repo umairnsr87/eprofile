57071829175fd6a3299f4a238bbd495ad38e4f1a
umair ansari <umairnsr87@gmail.com>
2020-01-25 16:55:24 +0530

    added signin functionality and logout.

--- eprofile/settings.py
+++ eprofile/settings.py
@@ -126,6 +126,10 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
 LOGIN_REDIRECT_URL='home'
 
 LOGIN_URL='login'
+
--- eprofile_app/admin.py
+++ eprofile_app/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-from .models import profile
 # Register your models here.
-admin.site.register(profile)
